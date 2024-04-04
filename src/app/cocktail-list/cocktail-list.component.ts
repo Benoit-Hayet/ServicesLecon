d211de6056f58292d335990b21948d1f570f8835
Benoit-Hayet <hayetbenoit@gmail.com>
2024-04-04 14:43:44 +0200

    add model

--- src/app/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -1,12 +1,26 @@
+import { NgForOf } from '@angular/common';
+import {CocktailService} from "../services/cocktail.service";
 import { Component } from '@angular/core';
+import { Cocktail } from '../model/cocktail.model';
 
 @Component({
   selector: 'app-cocktail-list',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './cocktail-list.component.html',
   styleUrl: './cocktail-list.component.scss'
 })
 export class CocktailListComponent {
+cocktails: Cocktail[] = [];
+
+  constructor(private CocktailService: CocktailService) {
+  }
+
+  ngOnInit(): void {
+    this.getCocktails();
+  }
+  getCocktails(): void {
+    this.cocktails = this.CocktailService.getCocktails();
+  }
 
 }
